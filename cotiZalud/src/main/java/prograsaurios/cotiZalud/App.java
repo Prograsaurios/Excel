3461cda15eddd7f62fc91a0285360d68b85d1e7a
raguileoam <raguileoam@users.noreply.github.com>
2018-05-22 14:09:22 -0400

    Actualización

--- cotiZalud/src/main/java/prograsaurios/cotiZalud/App.java
+++
@@ -1,8 +0,0 @@
-package prograsaurios.cotiZalud;
-
-public class App {
-	public static void main(String[] args) {
-		Controlador controlador = new Controlador(new Cliente("Prograsaurio", 1000));
-		Vista vista = new Vista(controlador);
-	}
-}
